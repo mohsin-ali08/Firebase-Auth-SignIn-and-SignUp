6d10096fa94b0f247ad311fbe8717efa47e97251
𝐌𝐎𝐇𝐒𝐈𝐍-𝐀𝐋𝐈 <167536098+mohsin-ali08@users.noreply.github.com>
2024-07-31 13:03:04 -0700

    Update firebase.js

--- images/firebase.js
+++ images/firebase.js
@@ -1,28 +1,28 @@
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-  import { getAuth,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
+  // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+  // import { getAuth,
+  //   createUserWithEmailAndPassword,
+  //   onAuthStateChanged,
+  //   signInWithEmailAndPassword,
 
-   } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+  //  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-   const firebaseConfig = {
-    apiKey: "AIzaSyDseabs5L4lukLhBc6McyeX24ETHBwvEOw",
-    authDomain: "user-authentication-b874c.firebaseapp.com",
-    projectId: "user-authentication-b874c",
-    storageBucket: "user-authentication-b874c.appspot.com",
-    messagingSenderId: "291164069457",
-    appId: "1:291164069457:web:198efb73a6a53a09c26270",
-    measurementId: "G-G1CBZP0SSN"
-  };
+  //  const firebaseConfig = {
+  //   apiKey: "AIzaSyDseabs5L4lukLhBc6McyeX24ETHBwvEOw",
+  //   authDomain: "user-authentication-b874c.firebaseapp.com",
+  //   projectId: "user-authentication-b874c",
+  //   storageBucket: "user-authentication-b874c.appspot.com",
+  //   messagingSenderId: "291164069457",
+  //   appId: "1:291164069457:web:198efb73a6a53a09c26270",
+  //   measurementId: "G-G1CBZP0SSN"
+  // };
 
   
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+//   const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
   
 
 export {
